1e2670a111a98a48ce2099ca941e940d083f23cf
LucaVitale00 <luca16vitale2000@gmail.com>
2023-02-01 22:48:58 +0100

    first commit

---
+++ database/migrations/2023_02_01_171744_create_trains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table-> string('name',32);
+            $table-> string('wagon',32);
+            $table-> string('passengers',64);
+            $table-> integer('typology')-> unsigned();
+            $table-> integer('seats')-> unsigned();
+            $table-> integer('classes')-> unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
